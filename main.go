ec31a52e7df033ec70437b54a6ef7dfd16719014
Aaron Suarez <aaron.j.suarez1@gmail.com>
2021-05-17 18:03:03 -0500

    Break main down a little

--- main.go
+++ main.go
@@ -12,34 +12,14 @@ import (
 )
 
 func main() {
-	host := os.Getenv("POSTGRES_HOST")
-	port, e := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-
-	if e != nil {
-		fmt.Println("Postgres port is invalid. Using 5432 instead.")
-		port = 5432
-	}
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlconn)
-	CheckError(err)
+	db := connectToPostgres()
 
 	defer db.Close()
 
-	err = db.Ping()
+	err := db.Ping()
 	CheckError(err)
-	fmt.Println("Connected to database successfully")
-	fmt.Println("Starting web server...")
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/products/{id}", DeleteHandler).Methods("DELETE")
-	r.HandleFunc("/products", StockDisplayHandler).Methods("GET")
 
-	http.Handle("/", r)
-	http.ListenAndServe(":8090", r)
+	startServer()
 }
 
 /*
@@ -47,6 +27,7 @@ func main() {
  */
 func HomeHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Juice Inventory\n")
+
 }
 
 /*
@@ -63,6 +44,39 @@ func DeleteHandler(w http.ResponseWriter, req *http.Request) {
  */
 func StockDisplayHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "This is the inventory:\n")
+}
+
+/*
+ * Utility Functions
+ */
+
+func connectToPostgres() *sql.DB {
+	host := os.Getenv("POSTGRES_HOST")
+	port, e := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	if e != nil {
+		fmt.Println("Invalid port, using 5432 instead")
+		port = 5432
+	}
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	return db
+}
+
+func startServer() {
+	fmt.Println("Starting server...")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products/{id}", DeleteHandler).Methods("DELETE")
+	r.HandleFunc("/products", StockDisplayHandler).Methods("GET")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8090", r)
 }
 
 func CheckError(err error) {

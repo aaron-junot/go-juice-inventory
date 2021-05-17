04c9c349261421df9ce0aafc5325f617dda0309a
Aaron Suarez <aaron.j.suarez1@gmail.com>
2021-05-17 17:36:44 -0500

    Add postgres database

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN adduser --system --disabled-password app
 COPY . src/
 
 RUN go get -u github.com/gorilla/mux
+RUN go get github.com/lib/pq
 
 RUN cd src/ && go build -o ../bin
 
--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Starting server...")
+	host := os.Getenv("POSTGRES_HOST")
+	port, e := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	if e != nil {
+		fmt.Println("Postgres port is invalid. Using 5432 instead.")
+		port = 5432
+	}
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	defer db.Close()
+
+	err = db.Ping()
+	CheckError(err)
+	fmt.Println("Connected to database successfully")
+	fmt.Println("Starting web server...")
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/products/{id}", DeleteHandler).Methods("DELETE")
@@ -23,7 +47,6 @@ func main() {
  */
 func HomeHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Juice Inventory\n")
-
 }
 
 /*
@@ -40,4 +63,10 @@ func DeleteHandler(w http.ResponseWriter, req *http.Request) {
  */
 func StockDisplayHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "This is the inventory:\n")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
